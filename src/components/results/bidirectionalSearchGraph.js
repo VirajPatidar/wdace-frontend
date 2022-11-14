ac2e0afc0960ad023a0c0033d13849ce1d522d1b
Viraj Patidar <vrj381@gmail.com>
2022-11-14 12:26:05 +0530

    Bidirectional Search Graph

---
+++ src/components/results/bidirectionalSearchGraph.js
@@ -0,0 +1,97 @@
+import Graph from "react-graph-vis";
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+
+const options = {
+    nodes: {
+        shape: 'dot',
+        size: 25
+    },
+    layout: {
+        hierarchical: false,
+    },
+    edges: {
+        color: "#000000"
+    }
+};
+
+function randomColor() {
+    const red = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
+    const green = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
+    const blue = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
+    return `#${red}${green}${blue}`;
+}
+
+const BidirectionalSearchGraph = ({domain, child_nodes, parent_nodes}) => {
+
+    let graph_nodes=[]
+    let graph_edges=[]
+    let unique_nodes=[]
+
+
+    let child=0; let parent=0;
+    if(Object.keys(child_nodes).length !== 0){
+        child = 1;
+        for (let c in child_nodes) {
+            unique_nodes.push(String(c))
+            for (let i = 0; i < child_nodes[c].length; i++) {
+                unique_nodes.push(child_nodes[c][i])
+            }
+        }
+    }
+    if(Object.keys(parent_nodes).length !== 0){
+        parent = 1;
+        for (let p in parent_nodes) {
+            unique_nodes.push(String(p))
+            for (let i = 0; i < parent_nodes[p].length; i++) {
+                unique_nodes.push(parent_nodes[p][i])
+            }
+        }
+    }
+
+    function removeDuplicates(arr) {
+        return arr.filter((item,
+            index) => arr.indexOf(item) === index);
+    }
+
+    unique_nodes = removeDuplicates(unique_nodes)
+    for (let i = 0; i < unique_nodes.length; i++) {
+        graph_nodes.push({id: String(unique_nodes[i]), label: unique_nodes[i], color: randomColor()})
+    }
+
+    if(parent == 1){
+        for (let p in parent_nodes) {
+            for (let i = 0; i < parent_nodes[p].length; i++) {
+                graph_edges.push({from: String(p), to: String(parent_nodes[p][i])})
+            }
+        }
+    }
+
+    if(child == 1){
+        for (let c in child_nodes) {
+            for (let i = 0; i < child_nodes[c].length; i++) {
+                graph_edges.push({from: String(c), to: String(child_nodes[c][i])})
+            }
+        }
+    }
+
+    // eslint-disable-next-line
+    const [state, setState] = useState({
+        counter: graph_nodes.length,
+        graph: {
+            nodes: graph_nodes,
+            edges: graph_edges
+        }
+    })
+
+    const { graph } = state;
+
+    return (
+        <Box>
+            <Graph graph={graph} options={options} style={{ height: "320px", padding: "0px", marginBottom: "0px" }} />
+        </Box>
+    );
+
+}
+
+export default BidirectionalSearchGraph;
---
+++ src/components/results/results.js
@@ -0,0 +1,314 @@
+import React, { useState, forwardRef } from 'react';
+import { results } from '../../atoms';
+import { useRecoilValue } from 'recoil';
+import { useSearchParams } from 'react-router-dom';
+import DomainTopicGraph from './domainTopicGraph';
+import BidirectionalSearchGraph from './bidirectionalSearchGraph';
+
+
+//MUI
+import { Box, Container, Paper, Typography, Chip, Grid, Button } from '@mui/material';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { styled } from '@mui/material/styles';
+import { cyan } from '@mui/material/colors';
+import Dialog from "@mui/material/Dialog";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Slide from "@mui/material/Slide";
+import Iframe from "react-iframe";
+
+
+const Transition = forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const ListItem = styled('li')(({ theme }) => ({
+    margin: theme.spacing(0.5),
+}));
+
+const lightbg = cyan[50];
+
+
+const Results = () => {
+
+    const result = useRecoilValue(results);
+
+    const chipData = result.keywords;
+
+    const [searchParams] = useSearchParams();
+    const url = searchParams.get("url");
+
+    const [value, setValue] = React.useState('1');
+    const [open, setOpen] = useState(false);
+    const [modalHeader, setModalHeader] = useState("");
+    const [iframeLink, setIframeLink] = useState("");
+
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const handleLightHouseClickOpen = (url) => {
+        const encodedURL = `?url=${encodeURIComponent(url)}`
+        const lightHouseUrl = `https://lighthouse.microlink.io/?url=` + encodeURIComponent(`https://api.microlink.io/${encodedURL}&meta=false&insights=true`)
+
+        console.log(lightHouseUrl);
+
+        setOpen(true);
+        setModalHeader('Lighthouse Audit Report');
+        setIframeLink(lightHouseUrl);
+    };
+
+    const handleClickOpen = (url) => {
+        url = url.replace("://", "/")
+        const encodedURL = encodeURIComponent(url)
+        const sucuriUrl = `https://api.microlink.io?url=https%3A%2F%2Fsitecheck.sucuri.net%2Fresults%2F${encodedURL}&pdf=true&waitUntil=networkidle0&meta=false&embed=pdf.url`
+
+        console.log(sucuriUrl);
+
+        setOpen(true);
+        setModalHeader("Security Report");
+        setIframeLink(sucuriUrl);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+        setModalHeader("");
+        setIframeLink("");
+    };
+
+    var LbL2VecDomain = [
+        'E-Commerce',
+        'News',
+        'Blogs and Educational',
+        'Business website/Gaming or Others',
+        'Social Media and Entertainment'
+    ];
+    var main_domain = LbL2VecDomain[Math.floor(Math.random() * LbL2VecDomain.length)];
+
+
+    return (
+        <Container>
+            <Box mt={4}>
+                <Typography variant="h4">
+                    {main_domain}
+                </Typography>
+                <Typography variant="h6">
+                    {result.textual_data.title}
+                </Typography>
+            </Box>
+            <Box pt={3} pb={1} sx={{ width: '100%', typography: 'body1' }}>
+                <Paper style={{ backgroundColor: lightbg }} elevation={3}>
+                    <TabContext value={value}>
+                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                            <TabList onChange={handleChange} aria-label="lab API tabs example">
+                                <Tab label="Extractive Summary" value="1" />
+                                <Tab label="Main Text" value="2" />
+                            </TabList>
+                        </Box>
+                        <TabPanel value="1">{result.textual_data.extractive_summary}</TabPanel>
+                        <TabPanel value="2">{result.textual_data.mainText}</TabPanel>
+                    </TabContext>
+                </Paper>
+            </Box>
+
+            <Box pt={2} pb={1} sx={{ display: "flex", flexDirection: "row" }}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={12} md={6}>
+                        <Box sx={{ textAlign: "center" }} >
+                            <Paper style={{ backgroundColor: lightbg, height: 355 }} elevation={3}>
+                                <Typography pt={1} variant="h6" color="primary">Domain Topic Graph</Typography>
+                                <DomainTopicGraph domain={result.domain} topics={result.topics} />
+                            </Paper>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={6}>
+                        <Box sx={{ textAlign: "center" }} >
+                            <Paper style={{ backgroundColor: lightbg, height: 355 }} elevation={3}>
+                                <Typography pt={1} variant="h6" color="primary">Bidirectional Search Graph</Typography>
+                                <BidirectionalSearchGraph domain={result.domain} child_nodes={result.children_nodes} parent_nodes={result.parent_nodes} />
+                            </Paper>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Box>
+            <Box pt={2} pb={1} sx={{ display: "flex", flexDirection: "row" }}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={12} md={6}>
+                        <Box sx={{ textAlign: "center" }} >
+                            <Paper style={{ backgroundColor: lightbg, height: 355 }} elevation={3}>
+                                <Typography pt={1} variant="h6" color="primary">Web Preview</Typography>
+                            </Paper>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={6}>
+                        <Box sx={{ textAlign: "center" }} >
+                            <Paper style={{ backgroundColor: lightbg, height: 352, overflow: "auto" }} elevation={3}>
+                                <Typography pt={1} variant="h6" color="primary">Performace and Audit Reports</Typography>
+                                {/* <Typography px={2} py={2} sx={{ textAlign: "left" }}>
+                                    {result.textual_data.rawOriginalText}
+                                </Typography> */}
+                                <Button
+                                    sx={{ textTransform: 'none' }}
+                                    variant="outlined"
+                                    onClick={() =>
+                                        handleLightHouseClickOpen(url)
+                                    }
+                                >
+                                    Get Lighthouse Report
+                                </Button>
+                                &nbsp;&nbsp;&nbsp;
+                                <Button
+                                    sx={{ textTransform: 'none' }}
+                                    variant="outlined"
+                                    onClick={() =>
+                                        handleClickOpen(url)
+                                    }
+                                >
+                                    Get Security Report
+                                </Button>
+                            </Paper>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Box>
+
+            <Box pt={2} pb={1} sx={{ textAlign: "center" }}>
+                <Paper style={{ backgroundColor: lightbg }} elevation={3}>
+                    <Box><Typography pt={1} variant="h6" color="primary">Keywords &amp; Important Phrases</Typography></Box>
+                    <Paper
+                        elevation={0}
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            flexWrap: 'wrap',
+                            listStyle: 'none',
+                            p: 0.5,
+                            m: 0,
+                        }}
+                        component="ul"
+                        style={{ backgroundColor: lightbg }}
+
+                    >
+
+                        {chipData.map((data, index) => {
+
+                            return (
+                                <ListItem key={index}>
+                                    <Chip label={data} color="primary" />
+                                </ListItem>
+                            );
+                        })}
+                    </Paper>
+                </Paper>
+            </Box>
+
+            <Box pt={2} pb={1} sx={{ display: "flex", flexDirection: "row" }}>
+                <Grid container spacing={3}>
+                    {result.original_lang !== 'English' ?
+                        <Grid item xs={12} sm={12} md={6}>
+                            <Box sx={{ textAlign: "center" }} >
+                                <Paper style={{ backgroundColor: lightbg, maxHeight: 300, overflow: "auto" }} elevation={3}>
+                                    <Typography pt={1} variant="h6" color="primary">Raw Web Content ({result.original_lang})</Typography>
+                                    <Typography px={2} py={2} sx={{ textAlign: "left" }}>
+                                        {result.textual_data.rawOriginalText}
+                                    </Typography>
+                                </Paper>
+                            </Box>
+                        </Grid>
+                        :
+                        <Grid item xs={12} sm={12} md={12}>
+                            <Box sx={{ textAlign: "center" }} >
+                                <Paper style={{ backgroundColor: lightbg, maxHeight: 300, overflow: "auto" }} elevation={3}>
+                                    <Typography pt={1} variant="h6" color="primary">Raw Web Content</Typography>
+                                    <Typography px={2} py={2} sx={{ textAlign: "left" }}>
+                                        {result.textual_data.rawOriginalText}
+                                    </Typography>
+                                </Paper>
+                            </Box>
+                        </Grid>
+                    }
+                    {result.original_lang !== 'English' ?
+                        <Grid item xs={12} sm={12} md={6}>
+                            <Box sx={{ textAlign: "center" }} >
+                                <Paper style={{ backgroundColor: lightbg, maxHeight: 300, overflow: "auto" }} elevation={3}>
+                                    <Typography pt={1} variant="h6" color="primary">Translated Web Content</Typography>
+                                    <Typography px={2} py={2} sx={{ textAlign: "left" }}>
+                                        {result.textual_data.rawText}
+                                    </Typography>
+                                </Paper>
+                            </Box>
+                        </Grid>
+                        : null}
+                </Grid>
+            </Box>
+
+            <Box pt={2} pb={4} sx={{ textAlign: "center" }}>
+                <Paper style={{ backgroundColor: lightbg }} elevation={3}>
+                    <Typography pt={1} variant="h6" color="primary">Text Properties</Typography>
+                    <Typography px={2} sx={{ textAlign: "left" }}>
+                        Raw Web Content Length: {result.len.rawOriginalText_len}
+                    </Typography>
+                    {result.original_lang !== 'English' ?
+                        <Typography px={2} sx={{ textAlign: "left" }}>
+                            Translated Web Content Length: {result.len.rawText_len}
+                        </Typography>
+                        : null}
+                    <Typography px={2} sx={{ textAlign: "left" }}>
+                        Main Text Length: {result.len.mainText_len}
+                    </Typography>
+                    <Typography px={2} pb={2} sx={{ textAlign: "left" }}>
+                        Extractive Summary Length: {result.len.extractive_summary_len}
+                    </Typography>
+                </Paper>
+            </Box>
+
+            <Dialog
+                fullScreen
+                open={open}
+                onClose={handleClose}
+                TransitionComponent={Transition}
+            >
+                <AppBar sx={{ position: "relative" }}>
+                    <Toolbar>
+                        <Typography
+                            sx={{ ml: 2, flex: 1 }}
+                            variant="h6"
+                            component="div"
+                        >
+                            {modalHeader}
+                        </Typography>
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            onClick={handleClose}
+                            aria-label="close"
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                    </Toolbar>
+                </AppBar>
+                <Iframe
+                    url={iframeLink}
+                    width="100%"
+                    height="4500px"
+                    scrolling="yes"
+                    loading="lazy"
+                />
+            </Dialog>
+
+            {/* <Box>
+                <pre>{JSON.stringify(result.len, null, 4)}</pre>
+                <br />
+                {JSON.stringify(result.text)}
+            </Box> */}
+        </Container >
+    );
+}
+
+export default Results;
